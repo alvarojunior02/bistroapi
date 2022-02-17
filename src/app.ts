f448ddca2c8761500dc36dd0fda048dbe29c59e7
alvarojunior02 <alvaroamcjunior8@gmail.com>
2022-02-17 05:29:47 -0400

    Resolve error of CORS

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true, 
+    optionSuccessStatus:200
+}
 import router from './routes'
 
 const app = express()
 
 app.use(express.json())
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(router);
 
 export { app }
