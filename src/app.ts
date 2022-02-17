d135da3c61761c3aa9ac3ed8d1d1835f269e3479
alvarojunior02 <alvaroamcjunior8@gmail.com>
2022-02-17 12:05:45 -0400

    Other update cors options

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,14 @@ import router from './routes'
 
 const app = express()
 
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-    app.use(cors());
-});
-app.use(express.json());
+app.use(express.json())
+app.use(cors({
+    origin: '*', 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    maxAge: 3600,
+    credentials: true,
+    allowedHeaders: ['Content-Type'],
+}));
 app.use(router);
 
 export { app }
