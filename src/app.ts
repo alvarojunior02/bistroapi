1cc5edf33204c60cf6a69bacbcedda5148fa4ac6
alvarojunior02 <alvaroamcjunior8@gmail.com>
2022-02-17 05:44:41 -0400

    Resolve other error of CORS

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,11 @@
 import express from 'express'
 const cors = require('cors');
-const corsOpts = {
-    origin: '*',
-  
-    methods: [
-      'GET',
-      'POST',
-      'PUT',
-      'DELETE'
-    ],
-  
-    allowedHeaders: [
-      'Content-Type',
-    ],
-  };
 import router from './routes'
 
 const app = express()
 
 app.use(express.json())
-app.use(cors(corsOpts));
+app.use(cors({origin: 'http://localhost:3000'}));
 app.use(router);
 
 export { app }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { app } from "./app"
 import database from "./database/db"
 
-const port = 3333
+const port = process.env.PORT || 3333
 
 const server = app.listen(port, async () => {
     await database.sync();
