b9a35249b51d4480051be10163555af5a80fb33a
alvarojunior02 <alvaroamcjunior8@gmail.com>
2022-02-17 11:30:48 -0400

    Other update cors options

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,13 @@ const corsOptions = {
     methods: "GET,PUT,POST,DELETE"
 }
 
-app.use(express.json())
-app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 app.use(router);
 
 export { app }
