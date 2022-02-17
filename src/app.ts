cbef14183f3a4462f25f4298315ee14298ece835
alvarojunior02 <alvaroamcjunior8@gmail.com>
2022-02-17 05:41:42 -0400

    Resolve other error of CORS

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,25 @@
 import express from 'express'
 const cors = require('cors');
+const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+      'PUT',
+      'DELETE'
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
 import router from './routes'
 
 const app = express()
 
 app.use(express.json())
-app.use(cors());
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', 1);
-    next();
-});
+app.use(cors(corsOpts));
 app.use(router);
 
 export { app }
