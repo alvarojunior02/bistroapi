9e56d69097be4bb4ee154a86320ab3b4fb785a14
alvarojunior02 <alvaroamcjunior8@gmail.com>
2022-02-18 17:37:08 -0400

    Return to first version

--- src/routes.ts
+++ src/routes.ts
@@ -5,14 +5,7 @@ import cors from 'cors'
 
 const router = express.Router();
 
-router.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type: application/json");
-    next();
-});
-
 router.get('/', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
     res.send({message: "Welcome to SMG Bistro API"})
 })
 
